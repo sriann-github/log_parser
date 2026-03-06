a7943ff24141ff15fb5b80510f67b626deef2652
sriann-github <srivalli.chitrapu@gmail.com>
2026-03-06 13:15:26 -0800

    Upload data into Mongo db
    1. Read the csv file into a DF
    2. Upload the data from DF into the DB

--- log_store.py
+++ log_store.py
@@ -4,14 +4,49 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-envStr = os.getenv()
 
 mongo_uri = os.getenv("MONGO_URI")
 db_name = os.getenv("DB_NAME")
 collection_name = os.getenv("COLLECTION_NAME")
 
-print(mongo_uri, db_name, collection_name)
+client = MongoClient(mongo_uri)
+db = client[db_name]          # creates DB if not exists
+collection = db[collection_name]          # creates collection if not exists
 
+try:
+    client.admin.command('ping')
+    print("Connected to MongDB Atlas successfully!")
+except Exception as e:
+    print(f"Connection failed: {e}")
 
-db = client["log_processor"]          # creates DB if not exists
-collection = db["incidents"]          # creates collection if not exists
+df = pd.read_csv("data/incident_event_log.csv")
+print(df.shape)
+print(df.columns)
+
+### Data Cleaning
+df = df.replace("?", None)
+
+# Convert date columns to datetime
+date_columns = ['opened_at', 'sys_created_at', 'sys_updated_at', 'resolved_at', 'closed_at' ]
+for col in date_columns:
+    df[col] = df[col].astype(str).replace('NaT', None)
+
+df = df.where(pd.notnull(df), None)
+
+#Convert dataframe to list of dictionaries
+records = df.to_dict(orient="records")
+
+batch_size = 1000
+
+if collection.count_documents({}) > 0:
+    print(f"Data already exists! {collection.counts_documents({})} records found, skipping insert")
+else:
+    for i in range(0, len(records), batch_size):
+        try:
+            batch = records[i:i + batch_size]
+            collection.insert_many(batch)
+            print(f"Inserted records {i} to {i + len(batch)}")
+        except Exception as e:
+            print(f"Failed at batch {i}: {e}")
+
+print(f"Total records in DB: {collection.count_documents({})}")
